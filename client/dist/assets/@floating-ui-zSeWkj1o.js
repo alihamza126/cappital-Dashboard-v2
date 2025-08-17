e59d9572d9e0c2e660ae42a72c5bc944820d0dc0
alihamza126 <alihamzageo75@gmail.com>
2025-08-17 16:49:34 +0500

    fix

--- client/dist/assets/@floating-ui-zSeWkj1o.js
+++
@@ -1,1 +0,0 @@
-import"./react-BLQlKNq_.js";import"./react-dom-DmmE3xc2.js";
--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "/",
+    baseURL: "http://localhost:5100",
     withCredentials:true
 });
 
 export default axiosInstance;
-
-
-//add new
